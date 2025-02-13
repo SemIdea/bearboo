b63b5a1a15c123e9236d052a1ee6a8b442d7d590
SemIdeia <codorkman@gmail.com>
2025-02-13 18:24:42 -0300

    Creating public and protected procedure

--- src/server/createRouter.ts
+++ src/server/createRouter.ts
@@ -1,5 +1,23 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 
 import { Context } from "./createContext";
 
-export const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create();
+
+const publicProcedure = t.procedure;
+
+const protectedProcedure = t.procedure.use(function isAuthed(opts) {
+  if (!opts.ctx.user?.email) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+    });
+  }
+
+  return opts.next({
+    ctx: {
+      user: opts.ctx.user,
+    },
+  });
+});
+
+export { t, publicProcedure, protectedProcedure };
