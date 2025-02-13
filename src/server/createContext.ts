f1d3f7295dbc3d0dd54351c21d8c2ed7115bd134
SemIdeia <codorkman@gmail.com>
2025-02-13 18:24:05 -0300

    Testing simple middleware

--- src/server/createContext.ts
+++ src/server/createContext.ts
@@ -1,5 +1,14 @@
-export const createContext = async () => {
-  return {};
+import type { CreateNextContextOptions } from "@trpc/server/adapters/next";
+
+export const createContext = async (opts: CreateNextContextOptions) => {
+  const user = {
+    id: 1,
+    email: "teste@test.com",
+  };
+
+  return {
+    user,
+  };
 };
 
-export type Context = ReturnType<typeof createContext>;
+export type Context = Awaited<ReturnType<typeof createContext>>;
