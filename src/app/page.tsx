718ee493a0d7f6124eb5d681a9de8b8291a5056a
SemIdeia <codorkman@gmail.com>
2025-02-21 21:45:33 -0300

    Add route test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import { trpc } from "./_trpc/client";
+
 export default function Home() {
+  // const { data: configQuery } = trpc.config.get.useQuery();
+
+  const { data: testQuery } = trpc.auth.test.useQuery();
+
+  useEffect(() => {
+    console.log(testQuery);
+  }, [testQuery]);
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <h2>Hello World!</h2>
