a34a7026a25073d2ddd364a01c5fc566b60feaa2
SemIdeia <codorkman@gmail.com>
2025-02-21 22:12:46 -0300

    Adding simple register page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+import { useAuth } from "@/context/auth";
+
+export default function Register() {
+  const { register } = useAuth();
+
+  const [email, setEmail] = useState(`user${Date.now()}@example.com`);
+  const [password, setPassword] = useState(`pass${Date.now()}`);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    register({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Username"
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
