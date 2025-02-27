4c6365a39009cd9296c55650b4201c3297cd988b
SemIdeia <codorkman@gmail.com>
2025-02-27 12:56:57 +0000

    Removing unused code

--- src/server/drivers/snowflake.ts
+++ src/server/drivers/snowflake.ts
@@ -15,17 +15,4 @@ function GetTimestampFromID(id: string): {
   return decodeSnowflake(id);
 }
 
-// (async () => {
-//   const machineId = 1; // machine ID (0-1023)
-//   const snowflake = new Snowflake(machineId);
-
-//   const id1 = await snowflake.generate();
-//   console.log("encodeID", id1);
-//   // output: 7160521316708126720
-
-//   const decoded = decodeSnowflake(id1);
-//   console.log("decodeID", decoded);
-//   // output: { timestamp: '2024-02-06T05:12:47.730Z', machineId: '1', sequence: '0' }
-// })();
-
 export { GenerateSnowflakeUID, GetTimestampFromID };
