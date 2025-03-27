9a11a19f6959097b762d8afa7ca137f4a97866fe
SemIdeia <codorkman@gmail.com>
2025-03-27 17:57:00 -0300

    Adding find

--- src/server/routers/post.routes.ts
+++ src/server/routers/post.routes.ts
@@ -1,12 +1,29 @@
-import { protectedProcedure, t } from "../createRouter";
+import { protectedProcedure, publicProcedure, t } from "../createRouter";
 import { createPostController } from "../features/post/create/controller";
 import { deletePostController } from "../features/post/delete/controller";
-import { createPostSchema, deletePostSchema } from "../schema/post.schema";
+import {
+  createPostSchema,
+  deletePostSchema,
+  findPostSchema,
+  updatePostSchema,
+} from "../schema/post.schema";
+import { findPostController } from "../features/post/find/controller";
+import { updatePostController } from "../features/post/update/controller";
+import { findAllPostsController } from "../features/post/findAll/controller";
 
 const PostRouter = t.router({
   createPost: protectedProcedure
     .input(createPostSchema)
     .mutation(async ({ input, ctx }) => createPostController({ input, ctx })),
+  findPost: protectedProcedure
+    .input(findPostSchema)
+    .query(async ({ input, ctx }) => findPostController({ input, ctx })),
+  findAllPost: publicProcedure.query(async ({ ctx }) =>
+    findAllPostsController({ ctx })
+  ),
+  updatePost: protectedProcedure
+    .input(updatePostSchema)
+    .mutation(async ({ input, ctx }) => updatePostController({ input, ctx })),
   deletePost: protectedProcedure
     .input(deletePostSchema)
     .mutation(async ({ input, ctx }) => deletePostController({ input, ctx })),
