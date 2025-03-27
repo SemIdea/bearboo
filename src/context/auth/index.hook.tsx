642e6e2ee404f84ee251d09ed564906350fe4d87
SemIdeia <codorkman@gmail.com>
2025-03-27 17:52:22 -0300

    Adding session to cookies

--- src/context/auth/index.hook.tsx
+++ src/context/auth/index.hook.tsx
@@ -1,14 +1,15 @@
-import { Session } from "@prisma/client";
 import { useState } from "react";
 import { trpc } from "@/app/_trpc/client";
+import { ISessionWithUser } from "@/server/entities/session/DTO";
 
 const useAuthLogic = () => {
-  const [session, setSession] = useState<Partial<Session> | null>(null);
+  const [session, setSession] = useState<ISessionWithUser | null>(null);
 
   const { mutate: login } = trpc.auth.loginUser.useMutation({
     onSuccess: (data) => {
       setSession(data);
       document.cookie = `accessToken=${data.accessToken}; path=/;`;
+      document.cookie = `session=${JSON.stringify(data)}; path=/;`;
       localStorage.setItem("refreshToken", data.refreshToken);
     },
   });
@@ -17,13 +18,14 @@ const useAuthLogic = () => {
     onSuccess: (data) => {
       setSession(data);
       document.cookie = `accessToken=${data.accessToken}; path=/;`;
+      document.cookie = `session=${JSON.stringify(data)}; path=/;`;
       localStorage.setItem("refreshToken", data.refreshToken);
     },
   });
 
   const logout = () => {};
 
-  return { session, login, register, logout };
+  return { session, setSession, login, register, logout };
 };
 
 type UseAuthLogicReturn = ReturnType<typeof useAuthLogic>;
--- src/context/auth/index.tsx
+++ src/context/auth/index.tsx
@@ -1,21 +1,33 @@
 "use client";
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { useAuthLogic, UseAuthLogicReturn } from "./index.hook";
 
 const AuthContext = createContext<UseAuthLogicReturn>({} as UseAuthLogicReturn);
 
-interface ChatProviderProps {
+type ChatProviderProps = {
   children: React.ReactNode;
-}
+};
 
 const Authprovider = ({ children }: ChatProviderProps) => {
-  const { session, login, register, logout } = useAuthLogic();
+  const { session, setSession, login, register, logout } = useAuthLogic();
+
+  useEffect(() => {
+    const sessionCookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("session="));
+
+    if (sessionCookie) {
+      const session = JSON.parse(sessionCookie.split("=")[1]);
+      setSession(session);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
         session,
+        setSession,
         login,
         register,
         logout,
