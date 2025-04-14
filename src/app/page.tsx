5015c76f09ac672824dd0477e2a3037a67d52ebc
SemIdeia <codorkman@gmail.com>
2025-04-14 18:19:52 -0300

    Fixing messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
-import { trpc } from "./_trpc/client";
 import { useAuth } from "@/context/auth";
-import { usePost } from "@/context/post";
 import { PostFeed } from "@/components/postFeed";
 
 const Home = () => {
   const { session } = useAuth();
-  const { findAllPosts } = usePost();
 
-  const { data: testQuery } = trpc.auth.test.useQuery();
-  const { data: posts, isLoading, error } = findAllPosts();
-
-  useEffect(() => {
-    console.log(testQuery);
-    console.log(posts);
-  }, [testQuery]);
+  // const { data: testQuery } = trpc.auth.test.useQuery();
 
   // useEffect(() => {
-  //   const posts = findAllPosts();
-  //   console.log(posts);
-  // }, [findAllPosts]);
+  //   console.log(testQuery);
+  // }, [testQuery]);
+
 
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <h2>Hello World!</h2>
-
-      <PostFeed />
-
-      <p>{JSON.stringify(session)}</p>
       <p>
         {session ? (
           <span>
@@ -39,6 +24,8 @@ const Home = () => {
           <span>You are not logged in</span>
         )}
       </p>
+
+      <PostFeed />
     </section>
   );
 };
