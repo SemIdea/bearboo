110df75882f3f749d450d940747f80445951f3a8
SemIdeia <codorkman@gmail.com>
2025-04-15 19:34:52 -0300

    Adding multiple get and set

--- src/server/integrations/repositories/cache/adapter.ts
+++ src/server/integrations/repositories/cache/adapter.ts
@@ -2,6 +2,8 @@ type ICacheRepositoryAdapter = {
   get: (key: string) => Promise<string | null>;
   set: (key: string, value: string, ex?: number) => Promise<void>;
   del: (key: string) => Promise<void>;
+  mget: (keys: string[]) => Promise<string[]>;
+  mset: (keys: string[], values: string[], ex?: number) => Promise<void>;
 };
 
 export type { ICacheRepositoryAdapter };
--- src/server/integrations/repositories/cache/implementations/redis.ts
+++ src/server/integrations/repositories/cache/implementations/redis.ts
@@ -24,6 +24,23 @@ class RedisCacheRepository implements ICacheRepositoryAdapter {
   async del(key: string): Promise<void> {
     await this.redisClient.del(key);
   }
+
+  async mget(keys: string[]): Promise<string[]> {
+    const values = await this.redisClient.mget(keys);
+    return values.filter((value) => value !== null) as string[];
+  }
+
+  async mset(keys: string[], values: string[], ex?: number): Promise<void> {
+    const multi = this.redisClient.multi();
+    keys.forEach((key, index) => {
+      if (ex) {
+        multi.set(key, values[index], "EX", ex);
+      } else {
+        multi.set(key, values[index]);
+      }
+    });
+    await multi.exec();
+  }
 }
 
 export { RedisCacheRepository };
