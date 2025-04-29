abbebadc73f5857c3000e8c16bc62fa28a45691c
SemIdeia <codorkman@gmail.com>
2025-04-29 16:28:00 -0300

    Adding a log in, register and create post link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/auth";
 import { PostFeed } from "@/components/postFeed";
 
 const Home = () => {
+  const router = useRouter();
   const { session } = useAuth();
 
   // const { data: testQuery } = trpc.auth.test.useQuery();
@@ -20,10 +23,24 @@ const Home = () => {
             You are logged in as <strong>{session.user.email}</strong>
           </span>
         ) : (
-          <span>You are not logged in</span>
+          <>
+            <span>You are not logged in</span>
+            <br />
+            <Link href={"auth/login"}>
+              <b>Log In</b>
+            </Link>
+            <br />
+            <Link href={"auth/register"}>
+              <b>Register</b>
+            </Link>
+          </>
         )}
       </p>
 
+      <Link href={"post/create"}>
+        <h2>Create post</h2>
+      </Link>
+      
       <PostFeed />
     </section>
   );
