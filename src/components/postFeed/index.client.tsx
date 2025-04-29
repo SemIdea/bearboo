e48bc27d2a021f56bf8fdd6a6f2fdf7d279321aa
SemIdeia <codorkman@gmail.com>
2025-04-29 16:32:00 -0300

    Adding a edit button and adding a <br />

--- src/components/postFeed/index.client.tsx
+++ src/components/postFeed/index.client.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/auth";
 import { usePost } from "@/context/post";
 
 const Posts = ({ max }: { max: number }) => {
+  const router = useRouter();
   const { findAllPosts } = usePost();
 
+  const { session } = useAuth();
   const { data: posts, isLoading, error } = findAllPosts();
 
   return (
@@ -21,9 +26,18 @@ const Posts = ({ max }: { max: number }) => {
           posts.length > 0 &&
           posts.slice(0, max).map((post) => (
             <div key={post.id}>
+              {post.userId == session?.user.id && (
+                <Link href={`/post/edit/${post.id}`}>
+                  <h3>
+                    <b>Edit</b>
+                  </h3>
+                </Link>
+              )}
               <h3>{post.title}</h3>
               <p>{post.content}</p>
               <p>{post.userId}</p>
+              <p>{post.id}</p>
+              <br />
             </div>
           ))}
       </div>
