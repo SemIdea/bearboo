72d7c642a7532f2d764afba19afe91152a6bb77f
SemIdeia <codorkman@gmail.com>
2025-06-09 20:52:31 -0300

    Implementing delete post controller testing

---
+++ src/server/features/post/delete/controller.test.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from "vitest";
+import { deletePostController } from "./controller";
+import { isControllerContext, TestContext } from "@/test/context";
+import { PostEntity } from "@/server/entities/post/entity";
+
+describe("Delete Post Controller Unitary Testing", async () => {
+  const ctx = new TestContext();
+
+  await ctx.createAuthenticatedUser();
+
+  if (!isControllerContext(ctx)) {
+    throw new Error("User not authenticated");
+  }
+
+  test("Should delete a post successfully", async () => {
+    const postId = await ctx.generateSnowflakeUuid();
+    const post = await PostEntity.create({
+      id: postId,
+      data: {
+        title: "Test Post",
+        content: "This is a test post.",
+        userId: ctx.user.id,
+      },
+      repositories: {
+        ...ctx.repositories,
+        database: ctx.repositories.post,
+      },
+    });
+
+    await deletePostController({
+      ctx,
+      input: {
+        postId: post.id,
+      },
+    });
+
+    const deletePost = await ctx.repositories.post.find(post.id);
+
+    expect(deletePost).toBeNull();
+  });
+});
