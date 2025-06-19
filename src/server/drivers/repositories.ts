2760037c72e4f137c3c8ab166923a3526d8e2ab6
SemIdeia <codorkman@gmail.com>
2025-06-19 14:20:35 -0300

    feat: adding `commentRepository` to the tRPC context

--- src/server/drivers/repositories.ts
+++ src/server/drivers/repositories.ts
@@ -3,6 +3,7 @@ import { PrismaUserModel } from "../entities/user/repositories/prisma";
 import { PrismaSessionModel } from "../entities/session/repositories/prisma";
 import { BycryptPasswordHashingHelper } from "../integrations/helpers/passwordHashing/implementations/bycrypt";
 import { PrismaPostModel } from "../entities/post/repositories/prisma";
+import { PrismaCommentModel } from "../entities/comment/repositories/prisma";
 
 // Singleton instances
 const cacheRepository = new RedisCacheRepository();
@@ -10,11 +11,13 @@ const userRepository = new PrismaUserModel();
 const sessionRepository = new PrismaSessionModel();
 const passwordHashingHelper = new BycryptPasswordHashingHelper();
 const postRepository = new PrismaPostModel();
+const commentRepository = new PrismaCommentModel();
 
 export {
   cacheRepository,
   userRepository,
   sessionRepository,
   passwordHashingHelper,
-  postRepository
+  postRepository,
+  commentRepository
 };
