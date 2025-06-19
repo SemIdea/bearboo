02abdcb20f8d93628578df0de4913af54a61b38b
SemIdeia <codorkman@gmail.com>
2025-06-19 14:23:18 -0300

    feat: implementing `PrismaCommentModel`

---
+++ src/server/entities/comment/repositories/prisma.ts
@@ -0,0 +1,50 @@
+import { ICommentEntity, ICommentModel } from "../DTO";
+import { prisma } from "@/server/drivers/prisma";
+
+class PrismaCommentModel implements ICommentModel {
+  async create(id: string, data: ICommentEntity) {
+    return await prisma.comment.create({
+      data: {
+        id,
+        ...data
+      }
+    });
+  }
+
+  async findById(id: string) {
+    return await prisma.comment.findUnique({
+      where: {
+        id
+      }
+    });
+  }
+
+  async findAllByPostId(postId: string) {
+    return await prisma.comment.findMany({
+      where: {
+        postId
+      }
+    });
+  }
+
+  async update(id: string, data: Partial<ICommentEntity>) {
+    return await prisma.comment.update({
+      where: {
+        id
+      },
+      data: {
+        ...data
+      }
+    });
+  }
+
+  async delete(id: string) {
+    await prisma.comment.delete({
+      where: {
+        id
+      }
+    });
+  }
+}
+
+export { PrismaCommentModel };
