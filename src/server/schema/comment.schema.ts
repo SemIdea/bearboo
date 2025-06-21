8a9faa56b506ecc6d78f7630fc7f25e61219dbac
SemIdeia <codorkman@gmail.com>
2025-06-20 21:57:47 -0300

    feat: implementing update comment schema

--- src/server/schema/comment.schema.ts
+++ src/server/schema/comment.schema.ts
@@ -9,22 +9,30 @@ const findAllCommentsByPostSchema = z.object({
   postId: z.string()
 });
 
+const updateCommentSchema = z.object({
+  id: z.string(),
+  content: z.string().min(1, "Content cannot be empty")
+});
+
 const deleteCommentSchema = z.object({
   id: z.string()
 });
 
 type CreateCommentInput = z.TypeOf<typeof createCommentschema>;
 type FindAllCommentsByPostInput = z.TypeOf<typeof findAllCommentsByPostSchema>;
+type UpdateCommentInput = z.TypeOf<typeof updateCommentSchema>;
 type DeleteCommentInput = z.TypeOf<typeof deleteCommentSchema>;
 
 export {
   createCommentschema,
   findAllCommentsByPostSchema,
+  updateCommentSchema,
   deleteCommentSchema
 };
 
 export type {
   CreateCommentInput,
   FindAllCommentsByPostInput,
+  UpdateCommentInput,
   DeleteCommentInput
 };
