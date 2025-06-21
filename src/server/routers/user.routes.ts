36806fa6365c0b266de25be60c3799e2df8cdfd2
SemIdeia <codorkman@gmail.com>
2025-06-21 03:00:01 -0300

    feat: implementing get user comments route

--- src/server/routers/user.routes.ts
+++ src/server/routers/user.routes.ts
@@ -1,7 +1,9 @@
 import { publicProcedure, t } from "../createRouter";
+import { getUserCommentsController } from "../features/user/comments/controller";
 import { getUserPostsController } from "../features/user/posts/controller";
 import { getUserProfileController } from "../features/user/profile/controller";
 import {
+  getUserCommentsSchema,
   getUserPostsSchema,
   getUserProfileSchema
 } from "../schema/user.schema";
@@ -12,7 +14,10 @@ const UserRouter = t.router({
     .query(async ({ input, ctx }) => getUserProfileController({ input, ctx })),
   posts: publicProcedure
     .input(getUserPostsSchema)
-    .query(async ({ input, ctx }) => getUserPostsController({ input, ctx }))
+    .query(async ({ input, ctx }) => getUserPostsController({ input, ctx })),
+  comments: publicProcedure
+    .input(getUserCommentsSchema)
+    .query(async ({ input, ctx }) => getUserCommentsController({ input, ctx }))
 });
 
 export { UserRouter };
