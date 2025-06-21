ebb17c93c8b9768fff5444f8bab18af25637d6fe
SemIdeia <codorkman@gmail.com>
2025-06-20 21:57:25 -0300

    feat: implementing `updateComment` route

--- src/server/routers/comment.routes.ts
+++ src/server/routers/comment.routes.ts
@@ -2,10 +2,12 @@ import { protectedProcedure, publicProcedure, t } from "../createRouter";
 import { createCommentController } from "../features/comment/create/controller";
 import { deleteCommentController } from "../features/comment/delete/controller";
 import { findAllCommentsByPostController } from "../features/comment/find/controller";
+import { updateCommentController } from "../features/comment/update/controller";
 import {
   createCommentschema,
   deleteCommentSchema,
-  findAllCommentsByPostSchema
+  findAllCommentsByPostSchema,
+  updateCommentSchema
 } from "../schema/comment.schema";
 
 const CommentRouter = t.router({
@@ -19,6 +21,11 @@ const CommentRouter = t.router({
     .query(async ({ input, ctx }) =>
       findAllCommentsByPostController({ input, ctx })
     ),
+  updateComment: protectedProcedure
+    .input(updateCommentSchema)
+    .mutation(async ({ input, ctx }) => {
+      updateCommentController({ input, ctx });
+    }),
   deleteComment: protectedProcedure
     .input(deleteCommentSchema)
     .mutation(async ({ input, ctx }) => deleteCommentController({ input, ctx }))
