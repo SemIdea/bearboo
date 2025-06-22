f6313e599ffba5916c861e55d0e90679cbe85cc6
SemIdeia <codorkman@gmail.com>
2025-06-22 20:34:32 -0300

    fix: avoid redundant user fetch in `CreateCommentService`

--- src/server/features/comment/create/service.ts
+++ src/server/features/comment/create/service.ts
@@ -1,29 +1,11 @@
-import { TRPCError } from "@trpc/server";
 import { ICreateCommentDTO } from "./DTO";
 import { CommentEntity } from "@/server/entities/comment/entity";
-import { UserEntity } from "@/server/entities/user/entity";
-import { UserErrorCode } from "@/shared/error/user";
 import { GenerateSnowflakeUID } from "@/server/drivers/snowflake";
 
 const CreateCommentService = async ({
   repositories,
   ...data
 }: ICreateCommentDTO) => {
-  const user = await UserEntity.find({
-    id: data.userId,
-    repositories: {
-      ...repositories,
-      database: repositories.user
-    }
-  });
-
-  if (!user) {
-    throw new TRPCError({
-      code: "NOT_FOUND",
-      message: UserErrorCode.USER_NOT_FOUD
-    });
-  }
-
   const commentId = await GenerateSnowflakeUID();
 
   const comment = await CommentEntity.create({
