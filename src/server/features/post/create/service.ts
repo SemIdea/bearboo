9ba6fb78c8fd013b0a73ec132192d0fe592896b4
SemIdeia <codorkman@gmail.com>
2025-06-22 20:34:46 -0300

    fix: avoid redundant user fetch in `CreatePostService`

--- src/server/features/post/create/service.ts
+++ src/server/features/post/create/service.ts
@@ -1,26 +1,8 @@
-import { TRPCError } from "@trpc/server";
 import { ICreatePostDTO } from "./DTO";
-import { UserEntity } from "@/server/entities/user/entity";
 import { PostEntity } from "@/server/entities/post/entity";
 import { GenerateSnowflakeUID } from "@/server/drivers/snowflake";
-import { UserErrorCode } from "@/shared/error/user";
 
 const CreatePostService = async ({ repositories, ...data }: ICreatePostDTO) => {
-  const user = await UserEntity.find({
-    id: data.userId,
-    repositories: {
-      database: repositories.user,
-      cache: repositories.cache
-    }
-  });
-
-  if (!user) {
-    throw new TRPCError({
-      code: "NOT_FOUND",
-      message: UserErrorCode.USER_NOT_FOUD
-    });
-  }
-
   const postId = await GenerateSnowflakeUID();
 
   const post = await PostEntity.create({
