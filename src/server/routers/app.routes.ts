213ef4bb253e5282e024976945ccd0afa36f0774
SemIdeia <codorkman@gmail.com>
2025-06-25 20:55:14 -0300

    chore: adding session router

--- src/server/routers/app.routes.ts
+++ src/server/routers/app.routes.ts
@@ -2,13 +2,15 @@ import { t } from "../createRouter";
 import { AuthRouter } from "./auth.routes";
 import { CommentRouter } from "./comment.routes";
 import { PostRouter } from "./post.routes";
+import { SessionRouter } from "./session.routes";
 import { UserRouter } from "./user.routes";
 
 const appRouter = t.router({
   auth: AuthRouter,
   post: PostRouter,
   user: UserRouter,
-  comment: CommentRouter
+  comment: CommentRouter,
+  session: SessionRouter
 });
 
 type AppRouter = typeof appRouter;
