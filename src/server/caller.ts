dffa016d919eb39d76dc0166351f5ec11c905787
SemIdeia <codorkman@gmail.com>
2025-06-25 20:59:44 -0300

    chore(caller): implementing createDynamicCaller to make calls on protectedProcudures

--- src/server/caller.ts
+++ src/server/caller.ts
@@ -1,8 +1,46 @@
 import { appRouter } from "./routers/app.routes";
 import { createTRPCContext } from "./createContext";
+import { cookies } from "next/headers";
+import { TRPCError } from "@trpc/server";
+import { AuthErrorCode } from "@/shared/error/auth";
+import { redirect } from "next/navigation";
 
-const createCaller = () => {
-  return appRouter.createCaller(createTRPCContext({}));
+type ICreateDynamicCallerDTO = {
+  pathName: string;
 };
 
-export { createCaller };
+const createCaller = async () => {
+  return appRouter.createCaller(
+    await createTRPCContext({
+      headers: new Headers()
+    })
+  );
+};
+
+const createDynamicCaller = async ({ pathName }: ICreateDynamicCallerDTO) => {
+  const cookieHeader = (await cookies())
+    .getAll()
+    .map(({ name, value }) => `${name}=${value}`)
+    .join(";");
+
+  const callerCtx = await createTRPCContext({
+    headers: new Headers({ cookie: cookieHeader })
+  });
+
+  const caller = appRouter.createCaller(callerCtx, {
+    onError: ({ error }) => {
+      if (error instanceof TRPCError) {
+        if (error.message === AuthErrorCode.SESSION_EXPIRED) {
+          redirect(`/auth/refresh?redirect=${pathName}`);
+        }
+      }
+    }
+  });
+
+  return {
+    caller,
+    ctx: callerCtx
+  };
+};
+
+export { createCaller, createDynamicCaller };
