7389e53a7a55c5ee38fde33bc13061fe39bdb536
SemIdeia <codorkman@gmail.com>
2025-07-05 02:55:33 -0300

    feat: implementing verify page

---
+++ src/app/auth/verify/page.tsx
@@ -0,0 +1,17 @@
+import { VerifyForm } from "./page.client";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1>Verify Your Email</h1>
+      <p>Enter the verification code you received by email.</p>
+      <p>
+        If you clicked the link in the email, the verification should happen
+        automatically.
+      </p>
+      <VerifyForm />
+    </div>
+  );
+};
+
+export default Page;
