edba541c8747edb22f8832deaa3e1989f9c63c78
SemIdeia <codorkman@gmail.com>
2025-07-12 12:25:39 -0300

    chore: implementing MDEditor on edit post page

--- src/app/post/edit/[id]/page.tsx
+++ src/app/post/edit/[id]/page.tsx
@@ -11,7 +11,7 @@ const Page = async (props: PageProps) => {
   const params = await props.params;
   const { id } = params;
 
-  const { caller, ctx } = await createDynamicCaller({
+  const { caller } = await createDynamicCaller({
     pathName: `/post/edit/${id}`
   });
 
@@ -20,7 +20,6 @@ const Page = async (props: PageProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <h2>Edit Post</h2>
-      {JSON.stringify(ctx.user, null, 2)}
       <UpdatePostForm post={post} />
     </div>
   );
