4d777ec346ba58146d9721ea25e18472a184d916
SemIdeia <codorkman@gmail.com>
2025-07-15 15:42:35 -0300

    chore: implementing new refresh page

--- src/app/auth/refresh/page.tsx
+++ src/app/auth/refresh/page.tsx
@@ -3,9 +3,13 @@ import SessionRefresher from "./page.client";
 
 const Page = () => {
   return (
-    <Suspense>
-      <SessionRefresher />
-    </Suspense>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Suspense>
+          <SessionRefresher />
+        </Suspense>
+      </div>
+    </div>
   );
 };
 
