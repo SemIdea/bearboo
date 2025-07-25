a289fd948d9c8227964f8e7ae7119bd40dbf4c36
SemIdeia <codorkman@gmail.com>
2025-07-25 10:29:12 -0300

    refactor: improving error codes

--- src/shared/error/comment.ts
+++ src/shared/error/comment.ts
@@ -1,11 +1,16 @@
 enum CommentErrorCode {
   COMMENT_NOT_FOUND = "COMMENT_NOT_FOUND",
-  COMMENT_NOT_BELONG_TO_USER = "COMMENT_NOT_BELONG_TO_USER"
+  COMMENT_DELETE_FORBIDDEN = "COMMENT_DELETE_FORBIDDEN",
+  COMMENT_UPDATE_FORBIDDEN = "COMMENT_UPDATE_FORBIDDEN"
 }
 
 const CommentErrorMessages = {
-  [CommentErrorCode.COMMENT_NOT_FOUND]: "Comment not found. Please check the ID.",
-  [CommentErrorCode.COMMENT_NOT_BELONG_TO_USER]: "This comment does not belong to the user."
+  [CommentErrorCode.COMMENT_NOT_FOUND]:
+    "Comment not found. Please check the ID.",
+  [CommentErrorCode.COMMENT_DELETE_FORBIDDEN]:
+    "You are not allowed to delete this comment.",
+  [CommentErrorCode.COMMENT_UPDATE_FORBIDDEN]:
+    "You are not allowed to update this comment."
 } as const;
 
 export { CommentErrorCode, CommentErrorMessages };
