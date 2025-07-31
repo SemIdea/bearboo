3371723d36c7fd1f3fa8fb29a9008e36d90e30bb
SemIdeia <codorkman@gmail.com>
2025-07-30 23:32:30 -0300

    feat: creating reset password and send reset password email features

---
+++ src/server/features/auth/reset-password/controller.ts
@@ -0,0 +1,48 @@
+import { IAPIContextDTO } from "@/server/createContext";
+import {
+  ResetPasswordInput,
+  SendResetPasswordEmailInput
+} from "@/server/schema/resetPassword.schema";
+import { ResetPasswordService, SendResetPasswordEmailService } from "./service";
+
+const sendResetPasswordEmailController = ({
+  input,
+  ctx
+}: {
+  input: SendResetPasswordEmailInput;
+  ctx: IAPIContextDTO;
+}) => {
+  const resetToken = SendResetPasswordEmailService({
+    email: input.email,
+    repositories: {
+      ...ctx.repositories,
+      database: ctx.repositories.user
+    },
+    helpers: ctx.helpers,
+    gateways: ctx.gateways
+  });
+
+  return resetToken;
+};
+
+const resetPasswordController = async ({
+  input,
+  ctx
+}: {
+  input: ResetPasswordInput;
+  ctx: IAPIContextDTO;
+}) => {
+  const user = await ResetPasswordService({
+    token: input.token,
+    newPassword: input.password,
+    repositories: {
+      ...ctx.repositories,
+      database: ctx.repositories.user
+    },
+    helpers: ctx.helpers
+  });
+
+  return user;
+};
+
+export { sendResetPasswordEmailController, resetPasswordController };
