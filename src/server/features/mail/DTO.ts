fbaca3c2f448fe1b108e2933d2853dcca61f5e1a
SemIdeia <codorkman@gmail.com>
2025-08-01 15:22:48 -0300

    feat: implementing send mail by user id

--- src/server/features/mail/DTO.ts
+++ src/server/features/mail/DTO.ts
@@ -1,3 +1,4 @@
+import { IUserModel } from "@/server/entities/user/DTO";
 import { IMailerGatewayAdapter } from "@/server/integrations/gateway/mailer/adapter";
 
 type ISendMailDTO = {
@@ -9,4 +10,16 @@ type ISendMailDTO = {
   };
 };
 
-export type { ISendMailDTO };
+type ISendMailByUserIdDTO = {
+  userId: string;
+  subject: string;
+  body: string;
+  repositories: {
+    database: IUserModel;
+  };
+  gateways: {
+    mail: IMailerGatewayAdapter;
+  };
+};
+
+export type { ISendMailDTO, ISendMailByUserIdDTO };
