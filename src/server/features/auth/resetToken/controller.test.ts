7ffda1444a55515ab3ca5eba78ba47789571f29a
SemIdea <codorkman@gmail.com>
2025-08-20 03:14:09 -0300

    chore: implementing reset user password controller unitary test

--- src/server/features/auth/resetToken/controller.test.ts
+++ src/server/features/auth/resetToken/controller.test.ts
@@ -1,6 +1,9 @@
 import { isControllerContext, TestContext } from "@/test/context";
 import { describe, expect, test } from "vitest";
-import { sendResetPasswordEmailController } from "./controller";
+import {
+  resetPasswordController,
+  sendResetPasswordEmailController
+} from "./controller";
 import { ResetTokenEntity } from "@/server/entities/resetToken/entity";
 
 describe("Reset Token Controller Unitary Testing", async () => {
@@ -33,5 +36,38 @@ describe("Reset Token Controller Unitary Testing", async () => {
     expect(resetToken?.userId).toBe(ctx.user.id);
     expect(result).toBeDefined();
     expect(result.success).toBe(true);
+  });
+
+  test("Should reset the user password", async () => {
+    const email = ctx.user.email;
+
+    await sendResetPasswordEmailController({
+      input: {
+        email
+      },
+      ctx
+    });
+
+    const resetToken = await ResetTokenEntity.readByUserId({
+      userId: ctx.user.id,
+      repositories: {
+        ...ctx.repositories,
+        database: ctx.repositories.resetToken
+      }
+    });
+
+    const input = {
+      token: resetToken!.token,
+      password: "NewPassword1234",
+      confirmPassword: "NewPassword1234"
+    };
+
+    const result = await resetPasswordController({
+      input,
+      ctx
+    });
+
+    expect(result).toBeDefined();
+    expect(result.password).not.toBe(ctx.user.password);
   });
 });
