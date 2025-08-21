35165d59ede4c344c1c786b4e364b869ab32a4c7
SemIdea <codorkman@gmail.com>
2025-08-20 23:52:21 -0300

    chore: add unit tests for read user from session and logout session error scenarios

--- src/server/features/auth/session/controller.test.ts
+++ src/server/features/auth/session/controller.test.ts
@@ -2,10 +2,14 @@ import { isControllerContext, TestContext } from "@/test/context";
 import { describe, expect, test } from "vitest";
 import {
   logoutUserFromSessionController,
+  readUserFromSessionController,
   refreshSessionController
 } from "./controller";
 import { SessionEntity } from "@/server/entities/session/entity";
 import { ISessionEntity } from "@/server/entities/session/DTO";
+import { TRPCError } from "@trpc/server";
+import { SessionErrorCode } from "@/shared/error/session";
+import { UserErrorCode } from "@/shared/error/user";
 
 describe("Refresh Session Controller Unitary Testing", async () => {
   const ctx = new TestContext();
@@ -40,6 +44,44 @@ describe("Refresh Session Controller Unitary Testing", async () => {
     expect(result.refreshToken).not.toBe(user.session.refreshToken);
     expect(result.userId).toBe(user.id);
   });
+
+  test("Should throw an error if token is invalid", async () => {
+    const input = {
+      refreshToken: "invalid-token"
+    };
+
+    await expect(
+      refreshSessionController({
+        input,
+        ctx
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "NOT_FOUND",
+        message: SessionErrorCode.INVALID_TOKEN
+      })
+    );
+  });
+});
+
+describe("Read User From Session Controller Unitary Testing", async () => {
+  const ctx = new TestContext();
+  await ctx.createAuthenticatedUser();
+
+  if (!isControllerContext(ctx)) {
+    throw new Error("User is not authenticated");
+  }
+
+  test("Should read user from session successfully", async () => {
+    const user = ctx.user;
+
+    const result = await readUserFromSessionController({
+      ctx
+    });
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(user);
+  });
 });
 
 describe("Logout Session Controller Unitary Testing", async () => {
@@ -67,5 +109,72 @@ describe("Logout Session Controller Unitary Testing", async () => {
     });
 
     expect(result).toBeNull();
+  });
+
+  test("Should throw an error if user is not found", async () => {
+    const ctx = new TestContext();
+    await ctx.createAuthenticatedUser();
+
+    if (!isControllerContext(ctx)) {
+      throw new Error("User is not authenticated");
+    }
+
+    ctx.user.id = "non-existent-user-id";
+
+    await expect(
+      logoutUserFromSessionController({
+        ctx
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "NOT_FOUND",
+        message: UserErrorCode.USER_NOT_FOUND
+      })
+    );
+  });
+
+  test("Should throw an error if session is not found", async () => {
+    const ctx = new TestContext();
+    await ctx.createAuthenticatedUser();
+
+    if (!isControllerContext(ctx)) {
+      throw new Error("User is not authenticated");
+    }
+
+    ctx.user.session.id = "non-existent-session-id";
+
+    await expect(
+      logoutUserFromSessionController({
+        ctx
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "NOT_FOUND",
+        message: SessionErrorCode.SESSION_NOT_FOUND
+      })
+    );
+  });
+
+  test("Should throw an error if session is not authorized", async () => {
+    const ctx = new TestContext();
+    await ctx.createAuthenticatedUser();
+
+    if (!isControllerContext(ctx)) {
+      throw new Error("User is not authenticated");
+    }
+
+    const session = ctx.user.session;
+    session.userId = "unauthorized-user-id";
+
+    await expect(
+      logoutUserFromSessionController({
+        ctx
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "FORBIDDEN",
+        message: SessionErrorCode.SESSION_NOT_AUTHORIZED
+      })
+    );
   });
 });
