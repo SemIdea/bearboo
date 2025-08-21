3de90ca429df11a5fe4202d2b905e70e4987d3b2
SemIdea <codorkman@gmail.com>
2025-08-21 10:38:09 -0300

    test: add error handling for non-existent posts and unauthorized updates in updatePostController

--- src/server/features/post/update/controller.test.ts
+++ src/server/features/post/update/controller.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from "vitest";
 import { updatePostController } from "./controller";
 import { PostEntity } from "@/server/entities/post/entity";
 import { isControllerContext, TestContext } from "@/test/context";
+import { TRPCError } from "@trpc/server";
+import { PostErrorCode } from "@/shared/error/post";
 
 describe("Update Post Controller Unitary Testing", async () => {
   const ctx = new TestContext();
@@ -42,5 +44,61 @@ describe("Update Post Controller Unitary Testing", async () => {
       title: "Updated Title",
       updatedAt: result.updatedAt
     });
+  });
+
+  test("Should throw error if post does not exist", async () => {
+    const input = {
+      id: "non-existent-id",
+      content: "Updated Content",
+      title: "Updated Title"
+    };
+
+    await expect(
+      updatePostController({
+        ctx,
+        input
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "NOT_FOUND",
+        message: PostErrorCode.POST_NOT_FOUND
+      })
+    );
+  });
+
+  test("Should throw error if user tries to update a post they do not own", async () => {
+    const otherUser = await ctx.createNewUser();
+
+    const id = ctx.helpers.uid.generate();
+    await PostEntity.create({
+      id,
+      data: {
+        title: "Title",
+        content: "Content",
+        userId: otherUser.id
+      },
+      repositories: {
+        ...ctx.repositories,
+        database: ctx.repositories.post
+      }
+    });
+
+    const input = {
+      id,
+      content: "Updated Content",
+      title: "Updated Title"
+    };
+
+    await expect(
+      updatePostController({
+        ctx,
+        input
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "FORBIDDEN",
+        message: PostErrorCode.POST_UPDATE_FORBIDDEN
+      })
+    );
   });
 });
