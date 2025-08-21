e24e3b7dfc700dab0ec9203b77e7d83940826162
SemIdea <codorkman@gmail.com>
2025-08-21 15:56:34 -0300

    feat: adding forgot password link to login form

--- src/app/(smal)/auth/login/page.tsx
+++ src/app/(smal)/auth/login/page.tsx
@@ -4,20 +4,29 @@ import Link from "next/link";
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-4">
       <CardBase
         title="Login to your account"
         description="Enter your email below to login to your account"
         content={
           <>
             <LoginForm />
-            <div className="mt-4 text-center text-sm">
+            <div className="mt-2 text-center text-sm">
               Don't have an account?{" "}
               <Link
                 href="/auth/register"
                 className="underline underline-offset-4"
               >
                 Sign up
+              </Link>
+            </div>
+            <div className="mt-2 text-center text-sm">
+              Forgot password?{" "}
+              <Link
+                href="/auth/recover"
+                className="underline underline-offset-4"
+              >
+                Reset it
               </Link>
             </div>
           </>
