347d5dfdbeedc69eee717f2251992f898b5611b4
SemIdea <codorkman@gmail.com>
2025-08-21 10:34:39 -0300

    test: add error handling for non-existent posts and unauthorized post updates in revalidate post

--- src/server/features/post/revalidate/controller.test.ts
+++ src/server/features/post/revalidate/controller.test.ts
@@ -3,6 +3,8 @@ import { revalidatePostController } from "./controller";
 import { isControllerContext, TestContext } from "@/test/context";
 import { revalidatePath } from "next/cache";
 import { PostEntity } from "@/server/entities/post/entity";
+import { TRPCError } from "@trpc/server";
+import { PostErrorCode } from "@/shared/error/post";
 
 vi.mock("next/cache", () => ({
   revalidatePath: vi.fn()
@@ -43,5 +45,54 @@ describe("Revalidate Post Controller Unitary Testing", async () => {
     expect(result.userId).toBe(ctx.user.id);
 
     expect(revalidatePath).toHaveBeenCalledWith(`/post/${post.id}`);
+  });
+
+  test("Should throw an error if post does not exist", async () => {
+    const input = {
+      id: "non-existent-post-id"
+    };
+
+    await expect(
+      revalidatePostController({
+        ctx,
+        input
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "NOT_FOUND",
+        message: PostErrorCode.POST_NOT_FOUND
+      })
+    );
+  });
+
+  test("Should throw an error if user is not the owner of the post", async () => {
+    const otherUser = await ctx.createNewUser();
+
+    const postId = ctx.helpers.uid.generate();
+    await PostEntity.create({
+      id: postId,
+      data: {
+        userId: otherUser.id,
+        title: "Another User's Post",
+        content: "This post belongs to another user."
+      },
+      repositories: { ...ctx.repositories, database: ctx.repositories.post }
+    });
+
+    const input = {
+      id: postId
+    };
+
+    await expect(
+      revalidatePostController({
+        ctx,
+        input
+      })
+    ).rejects.toThrowError(
+      new TRPCError({
+        code: "FORBIDDEN",
+        message: PostErrorCode.POST_UPDATE_FORBIDDEN
+      })
+    );
   });
 });
