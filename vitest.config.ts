07f76d8da926b8e5a8b5caec6f48f960af5cb6bf
SemIdea <codorkman@gmail.com>
2026-07-04 22:44:10 -0300

    chore: fix npm audit vulnerabilities

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +10,8 @@ export default defineConfig({
 
 	test: {
 		include: ["src/**/__test__/**/*.ts"],
-		poolOptions: {
-			threads: {
-				singleThread: true,
-			},
-		},
+		pool: "threads",
+		maxWorkers: 1,
+		fileParallelism: false,
 	},
 });
